a4fc5538a5f3c666407c1ac721f40789a21c726e
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-04-14 16:53:38 +0100

    Disabling Submit Button, ngForm

--- app/contact-form.component.ts
+++ app/contact-form.component.ts
@@ -7,8 +7,8 @@ import {Component} from 'angular2/core';
     templateUrl: 'app/contact-form.component.html'    
 })
 export class ContactFormComponent {  
-    log(x){
-        console.log(x);
+    onSubmit(form){
+        console.log(form);
     }        
  }
 
