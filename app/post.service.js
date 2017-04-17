6ca625f808b2dea1c35effc91ff4235c85bbb630
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-04-17 11:41:20 +0100

    Sending data to Server

--- app/post.service.js
+++ app/post.service.js
@@ -9,9 +9,14 @@ System.register(['rxjs/add/operator.map'], function(exports_1, context_1) {
             PostService = (function () {
                 function PostService(_http) {
                     this._http = _http;
+                    this._url = "http://jsonplaceholder.typicode.com/posts";
                 }
                 PostService.prototype.getPost = function () {
-                    return this._http.get("http://jsonplaceholder.typicode.com/posts")
+                    return this._http.get(this._url)
+                        .map(function (res) { return res.json(); });
+                };
+                PostService.prototype.createPost = function (post) {
+                    return this._http.post(this._url, JSON.stringify(post))
                         .map(function (res) { return res.json(); });
                 };
                 return PostService;
