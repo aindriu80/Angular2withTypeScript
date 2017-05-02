f22e2abbe30f38202efae34f95abc48e67cf4c81
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-05-02 17:15:35 +0100

    Display list of users

--- app/boot.js
+++ app/boot.js
@@ -1,9 +1,12 @@
-System.register(['angular2/platform/browser', './app.component', 'angular2/router'], function(exports_1, context_1) {
+System.register(['angular2/http', 'angular2/platform/browser', './app.component', 'angular2/router'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var browser_1, app_component_1, router_1;
+    var http_1, browser_1, app_component_1, router_1;
     return {
         setters:[
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
             function (browser_1_1) {
                 browser_1 = browser_1_1;
             },
@@ -14,7 +17,7 @@ System.register(['angular2/platform/browser', './app.component', 'angular2/route
                 router_1 = router_1_1;
             }],
         execute: function() {
-            browser_1.bootstrap(app_component_1.AppComponent, [router_1.ROUTER_PROVIDERS]);
+            browser_1.bootstrap(app_component_1.AppComponent, [router_1.ROUTER_PROVIDERS, http_1.HTTP_PROVIDERS]);
         }
     }
 });
